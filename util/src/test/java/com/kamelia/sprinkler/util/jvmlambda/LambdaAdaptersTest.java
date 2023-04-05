aa402ca1c08dea4f251f1560db125062915856d5
ZwenDo <lorris.creantor@gmail.com>
2023-04-05 22:13:58 +0200

    Renamed tests

--- util/src/test/java/com/kamelia/sprinkler/util/jvmlambda/LambdaAdaptersTest.java
+++ util/src/test/java/com/kamelia/sprinkler/util/jvmlambda/LambdaAdaptersTest.java
@@ -9,14 +9,14 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public final class LambdaWrappingTest {
+public final class LambdaAdaptersTest {
 
     @Test
     public void wrapRunnable() {
         var list = new ArrayList<String>();
         var value = "hello";
 
-        var f = LambdaWrapping.w(() -> list.add(value));
+        var f = LambdaAdapters.a(() -> list.add(value));
         f.invoke();
 
         assertEquals(List.of(value), list);
@@ -27,7 +27,7 @@ public final class LambdaWrappingTest {
         var list = new ArrayList<String>();
         var value = "hello";
 
-        var f = LambdaWrapping.w((String s) -> list.add(s));
+        var f = LambdaAdapters.a((String s) -> list.add(s));
         f.invoke(value);
 
         assertEquals(List.of(value), list);
@@ -39,7 +39,7 @@ public final class LambdaWrappingTest {
         var value1 = "hello";
         var value2 = "world";
 
-        var f = LambdaWrapping.w((String s1, String s2) -> list.add(s1 + s2));
+        var f = LambdaAdapters.a((String s1, String s2) -> list.add(s1 + s2));
         f.invoke(value1, value2);
 
         assertEquals(List.of(value1 + value2), list);
@@ -47,7 +47,7 @@ public final class LambdaWrappingTest {
 
     @Test
     public void privateConstructorThrows() {
-        var constructor = LambdaWrapping.class.getDeclaredConstructors()[0];
+        var constructor = LambdaAdapters.class.getDeclaredConstructors()[0];
         constructor.setAccessible(true);
         var e = assertThrows(InvocationTargetException.class, constructor::newInstance);
         assertEquals(AssertionError.class, e.getTargetException().getClass());
