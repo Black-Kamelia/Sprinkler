b4441c085c846bf29be361aff4568993b9dba64d
ZwenDo <lorris.creantor@gmail.com>
2023-04-05 22:16:39 +0200

    Removed files

--- binary-serializers/src/main/kotlin/com/kamelia/sprinkler/codec/binary/bijection/BasicBijectionImpl.kt
+++
@@ -1,9 +0,0 @@
-package com.kamelia.sprinkler.codec.binary.bijection
-
-import com.kamelia.sprinkler.codec.binary.decoder.core.Decoder
-import com.kamelia.sprinkler.codec.binary.encoder.core.Encoder
-
-class BasicBijectionImpl<T>(
-    override val encoder: Encoder<T>,
-    override val decoderFactory: () -> Decoder<T>
-) : Bijection<T>
