39434f9b51f5ab7d84a74a970e8be7a443cd7d85
ZwenDo <lorris.creantor@gmail.com>
2023-04-05 23:08:57 +0200

    used lambda syntax for encoders when possible

--- binary-serializers/src/main/kotlin/com/kamelia/sprinkler/codec/binary/encoder/EncoderUtils.kt
+++ binary-serializers/src/main/kotlin/com/kamelia/sprinkler/codec/binary/encoder/EncoderUtils.kt
@@ -3,60 +3,45 @@
 package com.kamelia.sprinkler.codec.binary.encoder
 
 import com.kamelia.sprinkler.codec.binary.encoder.core.Encoder
-import com.kamelia.sprinkler.codec.binary.encoder.core.EncoderOutput
 
-inline fun <T, R> Encoder<T>.withMappedInput(crossinline mapper: (R) -> T): Encoder<R> = object : Encoder<R> {
+inline fun <T, R> Encoder<T>.withMappedInput(crossinline mapper: (R) -> T): Encoder<R> =
+    Encoder { obj, output -> this@withMappedInput.encode(mapper(obj), output) }
 
-    override fun encode(obj: R, output: EncoderOutput): Unit = this@withMappedInput.encode(mapper(obj), output)
-
-}
-
-fun <T> Encoder<T>.toIterable(endMarker: ByteArray): Encoder<Iterable<T>> = object : Encoder<Iterable<T>> {
-
-    override fun encode(obj: Iterable<T>, output: EncoderOutput) {
+fun <T> Encoder<T>.toIterable(endMarker: ByteArray): Encoder<Iterable<T>> {
+    require(endMarker.isNotEmpty()) { "End marker must not be empty" }
+    return Encoder { obj, output ->
         obj.forEach { this@toIterable.encode(it, output) }
         output.write(endMarker)
     }
-
 }
 
 @JvmOverloads
-fun <T> Encoder<T>.toCollection(
-    sizeEncoder: Encoder<Int> = IntEncoder(),
-): Encoder<Collection<T>> = object : Encoder<Collection<T>> {
-
-    override fun encode(obj: Collection<T>, output: EncoderOutput) {
+fun <T> Encoder<T>.toCollection(sizeEncoder: Encoder<Int> = IntEncoder()): Encoder<Collection<T>> =
+    Encoder { obj, output ->
         sizeEncoder.encode(obj.size, output)
         obj.forEach { this@toCollection.encode(it, output) }
     }
 
+@JvmOverloads
+fun <K, V> Encoder<Map.Entry<K, V>>.toMap(sizeEncoder: Encoder<Int> = IntEncoder()): Encoder<Map<K, V>> =
+    toCollection(sizeEncoder).withMappedInput { it.entries }
+
+fun <K, V> Encoder<Map.Entry<K, V>>.toMap(endMarker: ByteArray): Encoder<Map<K, V>> {
+    require(endMarker.isNotEmpty()) { "End marker must not be empty" }
+    return toIterable(endMarker).withMappedInput { it.entries }
+}
+
+fun <K, V> Encoder<K>.toMap(valueEncoder: Encoder<V>, sizeEncoder: Encoder<Int> = IntEncoder()): Encoder<Map<K, V>> =
+    toMapEntryEncoder(valueEncoder).toMap(sizeEncoder)
+
+fun <K, V> Encoder<K>.toMap(valueEncoder: Encoder<V>, endMarker: ByteArray): Encoder<Map<K, V>> {
+    require(endMarker.isNotEmpty()) { "End marker must not be empty" }
+    return toMapEntryEncoder(valueEncoder).toMap(endMarker)
 }
 
 @JvmOverloads
-fun <K, V> Encoder<Map.Entry<K, V>>.toMap(
-    sizeEncoder: Encoder<Int> = IntEncoder(),
-): Encoder<Map<K, V>> = toCollection(sizeEncoder).withMappedInput { it.entries }
-
-fun <K, V> Encoder<Map.Entry<K, V>>.toMap(endMarker: ByteArray): Encoder<Map<K, V>> =
-    toIterable(endMarker).withMappedInput { it.entries }
-
-fun <K, V> Encoder<K>.toMap(
-    valueEncoder: Encoder<V>,
-    sizeEncoder: Encoder<Int> = IntEncoder(),
-): Encoder<Map<K, V>> = toMapEntryEncoder(valueEncoder).toMap(sizeEncoder)
-
-fun <K, V> Encoder<K>.toMap(
-    valueEncoder: Encoder<V>,
-    endMarker: ByteArray,
-): Encoder<Map<K, V>> = toMapEntryEncoder(valueEncoder).toMap(endMarker)
-
-
-@JvmOverloads
-fun <T : Any> Encoder<T>.toOptional(
-    nullabilityEncoder: Encoder<Boolean> = BooleanEncoder(),
-): Encoder<T?> = object : Encoder<T?> {
-
-    override fun encode(obj: T?, output: EncoderOutput) {
+fun <T : Any> Encoder<T>.toOptional(nullabilityEncoder: Encoder<Boolean> = BooleanEncoder()): Encoder<T?> =
+    Encoder { obj, output ->
         val isNotNull = obj != null
         nullabilityEncoder.encode(isNotNull, output)
         if (isNotNull) {
@@ -64,27 +49,15 @@ fun <T : Any> Encoder<T>.toOptional(
         }
     }
 
-}
-
 @JvmOverloads
-fun <T> Encoder<T>.toArray(
-    sizeEncoder: Encoder<Int> = IntEncoder(),
-): Encoder<Array<T>> = object : Encoder<Array<T>> {
-
-    override fun encode(obj: Array<T>, output: EncoderOutput) {
+fun <T> Encoder<T>.toArray(sizeEncoder: Encoder<Int> = IntEncoder()): Encoder<Array<T>> =
+    Encoder { obj, output ->
         sizeEncoder.encode(obj.size, output)
         obj.forEach { this@toArray.encode(it, output) }
     }
 
-}
-
-private fun <K, V> Encoder<K>.toMapEntryEncoder(
-    valueEncoder: Encoder<V>,
-): Encoder<Map.Entry<K, V>> = object : Encoder<Map.Entry<K, V>> {
-
-    override fun encode(obj: Map.Entry<K, V>, output: EncoderOutput) {
+fun <K, V> Encoder<K>.toMapEntryEncoder(valueEncoder: Encoder<V>): Encoder<Map.Entry<K, V>> =
+    Encoder { obj, output ->
         this@toMapEntryEncoder.encode(obj.key, output)
         valueEncoder.encode(obj.value, output)
     }
-
-}
