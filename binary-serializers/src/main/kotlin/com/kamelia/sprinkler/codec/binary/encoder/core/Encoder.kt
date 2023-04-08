a98619316cd878dd3a424ea0bfd214bb9ff07e4c
ZwenDo <lorris.creantor@gmail.com>
2023-04-08 16:38:44 +0200

    Added doc

--- binary-serializers/src/main/kotlin/com/kamelia/sprinkler/codec/binary/encoder/core/Encoder.kt
+++ binary-serializers/src/main/kotlin/com/kamelia/sprinkler/codec/binary/encoder/core/Encoder.kt
@@ -5,20 +5,75 @@ import java.io.OutputStream
 import java.nio.file.Path
 import kotlin.io.path.outputStream
 
+/**
+ * Represents an object that can convert an object of type [T] to bytes.
+ *
+ * An encoder is stateless and encodes in a single operation, it can be instantiated once and reused multiple times to
+ * encode different objects.
+ *
+ * &nbsp;
+ *
+ * This interface has only one abstract method and therefore can be implemented as a lambda, as shown below:
+ *
+ * &nbsp;
+ *
+ * ```
+ * class MyBytePair(val first: Byte, val second: Byte)
+ *
+ * val myEncoder = Encoder<MyBytePair> { obj, output ->
+ *     output.write(obj.first)
+ *     output.write(obj.second)
+ * }
+ * ```
+ *
+ * @param T The type of the object to encode.
+ * @see EncoderOutput
+ */
 fun interface Encoder<in T> {
 
+    /**
+     * Encodes the given [obj] to the given [output].
+     *
+     * @param obj the object to encode
+     * @param output the output to write the encoded bytes to
+     * @see EncoderOutput
+     */
     fun encode(obj: T, output: EncoderOutput)
 
+    /**
+     * Encodes the given [obj] to a [ByteArray].
+     *
+     * @param obj the object to encode
+     * @return the encoded bytes
+     */
     fun encode(obj: T): ByteArray {
         val output = ArrayList<Byte>()
         encode(obj, output::add)
         return output.toByteArray()
     }
 
+    /**
+     * Encodes the given [obj] to the given [OutputStream].
+     *
+     * @param obj the object to encode
+     * @param output the output to write the encoded bytes to
+     */
     fun encode(obj: T, output: OutputStream): Unit = encode(obj, EncoderOutput.from(output))
 
+    /**
+     * Encodes the given [obj] to the given [Path].
+     *
+     * @param obj the object to encode
+     * @param output the output to write the encoded bytes to
+     */
     fun encode(obj: T, output: Path): Unit = output.outputStream().use { encode(obj, it) }
 
+    /**
+     * Encodes the given [obj] to the given [File].
+     *
+     * @param obj the object to encode
+     * @param output the output to write the encoded bytes to
+     */
     fun encode(obj: T, output: File): Unit = output.outputStream().use { encode(obj, it) }
 
 }
--- binary-serializers/src/main/kotlin/com/kamelia/sprinkler/codec/binary/encoder/core/EncoderOutput.kt
+++ binary-serializers/src/main/kotlin/com/kamelia/sprinkler/codec/binary/encoder/core/EncoderOutput.kt
@@ -1,37 +1,92 @@
 package com.kamelia.sprinkler.codec.binary.encoder.core
 
 import java.io.OutputStream
+import java.util.*
 
+/**
+ * Abstraction allowing [Encoders][Encoder] to write bytes. This interface provides methods for writing bytes in various
+ * ways, including writing a single byte, writing a [ByteArray], writing an [Iterable] of bytes, etc.
+ *
+ * To implement this interface, [write] is the only method must be implemented. All other methods are implemented in
+ * terms of this method, meaning that this interface is actually a functional interface and can be implemented as a
+ * lambda, as shown below:
+ *
+ * &nbsp;
+ *
+ * ```
+ * val myOutput = EncoderOutput { byte -> println(byte) } // myOutput.write(byte) will print the byte
+ * ```
+ *
+ * @see Encoder
+ */
 fun interface EncoderOutput {
 
+    /**
+     * Writes a single byte to the output.
+     *
+     * @param byte the byte to write
+     */
     fun write(byte: Byte)
 
+    /**
+     * Writes bytes from the given [ByteArray] to the output. The [start] and [length] parameters specify the range of
+     * indices in the [ByteArray] to write. The [start] parameter is inclusive, and the [length] parameter is exclusive.
+     *
+     * @param bytes the [ByteArray] to write
+     * @param start the inclusive start index in the [ByteArray] to write
+     * @param length the exclusive end index in the [ByteArray] to write
+     * @throws IndexOutOfBoundsException if [start] < 0 or [length] < 0 or [start] + [length] > [ByteArray.size]
+     */
     fun write(bytes: ByteArray, start: Int, length: Int) {
-        require(start >= 0) { "Start must be >= 0, was $start" }
-        require(length >= 0) { "Length must be >= 0, was $length" }
+        Objects.checkFromIndexSize(start, length, bytes.size)
         val lastIndex = start + length
-        require(lastIndex <= bytes.size) {
-            "start + length must be <= bytes.size, sum was ${lastIndex}, bytes.size was ${bytes.size}"
-        }
+
         for (index in start until lastIndex) {
             write(bytes[index])
         }
     }
 
+    /**
+     * Writes bytes from the given [ByteArray] to the output. The [start] parameter specifies the inclusive start index
+     * in the [ByteArray] to write. The method will write all bytes from the [start] index to the end of the
+     * [ByteArray].
+     *
+     * @param bytes the [ByteArray] to write
+     * @param start the inclusive start index in the [ByteArray] to write
+     */
     fun write(bytes: ByteArray, start: Int): Unit = write(bytes, start, bytes.size - start)
 
+    /**
+     * Writes bytes from the given [ByteArray] to the output. The method will write all bytes from the [ByteArray].
+     *
+     * @param bytes the [ByteArray] to write
+     */
     fun write(bytes: ByteArray): Unit = write(bytes, 0, bytes.size)
 
+    /**
+     * Writes bytes from the given [Iterable] of bytes to the output.
+     *
+     * @param bytes the [Iterable] of bytes to write
+     */
     fun write(bytes: Iterable<Byte>): Unit = bytes.forEach(::write)
 
     companion object {
 
+        /**
+         * Creates an [EncoderOutput] that writes to the given [OutputStream].
+         *
+         * @param output the [OutputStream] to write to
+         * @return the [EncoderOutput] that writes to the given [OutputStream]
+         */
         @JvmStatic
         fun from(output: OutputStream): EncoderOutput = object : EncoderOutput {
 
             override fun write(byte: Byte): Unit = output.write(byte.toInt())
 
-            override fun write(bytes: ByteArray, start: Int, length: Int) = output.write(bytes, start, length)
+            override fun write(bytes: ByteArray, start: Int, length: Int) {
+                Objects.checkFromIndexSize(start, length, bytes.size)
+                output.write(bytes, start, length)
+            }
 
         }
 
