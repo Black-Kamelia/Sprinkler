49bdeaf319564ae0129c5edf0aadba11488d821b
ZwenDo <lorris.creantor@gmail.com>
2023-04-13 20:37:05 +0200

    Added doc

--- binary-serializers/src/main/kotlin/com/kamelia/sprinkler/codec/binary/encoder/EncoderUtils.kt
+++ binary-serializers/src/main/kotlin/com/kamelia/sprinkler/codec/binary/encoder/EncoderUtils.kt
@@ -4,14 +4,54 @@ package com.kamelia.sprinkler.codec.binary.encoder
 
 import com.kamelia.sprinkler.codec.binary.encoder.core.Encoder
 
+/**
+ * Creates an encoder that encodes an object [R] using a given encoder of type [T]. The created encoder will map the
+ * input object [R] to an object of type [T] using the given [mapper] function, and then delegate the encoding to the
+ * original encoder.
+ *
+ * Here is an example of how to use this function:
+ *
+ * ```
+ * val longEncoder = LongEncoder()
+ * val instantEncoder: Encoder<Instant> = longEncoder.withMappedInput(Instant::toEpochMilli)
+ * ```
+ *
+ * The above code creates an encoder that encodes [Instant][java.time.Instant] objects by first converting them to a
+ * long value using [Instant.toEpochMilli][java.time.Instant.toEpochMilli], and then encoding the long value using the
+ * longEncoder.
+ *
+ * @receiver the original encoder of type [T]
+ * @param mapper the function that maps the input object [R] to an object of type [T]
+ * @return an encoder that encodes an object [R]
+ */
 fun <T, R> Encoder<T>.withMappedInput(mapper: (R) -> T): Encoder<R> =
     Encoder { obj, output -> this@withMappedInput.encode(mapper(obj), output) }
 
-fun <T> Encoder<T>.toIterable(endMarker: T): Encoder<Iterable<T>> = Encoder { obj, output ->
-    obj.forEach { this@toIterable.encode(it, output) }
-    this@toIterable.encode(endMarker, output)
-}
+/**
+ * Creates an encoder that encodes an [Iterable] of objects of type [T]. The created encoder will encode each object
+ * in the iterable using the original encoder, and then encode the [endMarker] using the original encoder. The end
+ * marker is used to indicate the end of the iterable.
+ *
+ * @receiver the original encoder of type [T]
+ * @param endMarker the end marker that is used to indicate the end of the iterable
+ * @return an encoder that encodes an [Iterable] of objects of type [T]
+ */
+fun <T> Encoder<T>.toIterable(endMarker: T): Encoder<Iterable<T>> =
+    Encoder { obj, output ->
+        obj.forEach { this@toIterable.encode(it, output) }
+        this@toIterable.encode(endMarker, output)
+    }
 
+/**
+ * Creates an encoder that encodes a [Collection] of objects of type [T]. The created encoder will encode the size of
+ * the collection using the given [sizeEncoder], and then encode each object in the collection using the original
+ * encoder.
+ *
+ * @receiver the original encoder of type [T]
+ * @param sizeEncoder the encoder that is used to encode the size of the collection (defaults to the default
+ * [IntEncoder])
+ * @return an encoder that encodes a [Collection] of objects of type [T]
+ */
 @JvmOverloads
 fun <T> Encoder<T>.toCollection(sizeEncoder: Encoder<Int> = IntEncoder()): Encoder<Collection<T>> =
     Encoder { obj, output ->
@@ -19,21 +59,101 @@ fun <T> Encoder<T>.toCollection(sizeEncoder: Encoder<Int> = IntEncoder()): Encod
         obj.forEach { this@toCollection.encode(it, output) }
     }
 
+/**
+ * Creates an encoder that encodes a [Map] of objects of type [K] and [V]. The created encoder will encode the size of
+ * the map using the given [sizeEncoder], and then encode each entry in the map as a [Pair] using the original encoder.
+ *
+ * @receiver the original encoder of [Pairs][Pair] of [K] and [V]
+ * @param sizeEncoder the encoder that is used to encode the size of the map (defaults to the default [IntEncoder])
+ * @return an encoder that encodes a [Map] of objects of type [K] and [V]
+ */
 @JvmOverloads
-fun <K, V> Encoder<Map.Entry<K, V>>.toMap(sizeEncoder: Encoder<Int> = IntEncoder()): Encoder<Map<K, V>> =
-    toCollection(sizeEncoder).withMappedInput(Map<K, V>::entries)
+fun <K, V> Encoder<Pair<K, V>>.toMap(sizeEncoder: Encoder<Int> = IntEncoder()): Encoder<Map<K, V>> =
+    Encoder { obj, output ->
+        sizeEncoder.encode(obj.size, output)
+        obj.forEach { (key, value) -> this@toMap.encode(key to value, output) }
+    }
 
-fun <K, V> Encoder<Map.Entry<K, V>>.toMap(endMarker: Pair<K, V>): Encoder<Map<K, V>> {
-    return toIterable(endMarker.toEntry()).withMappedInput(Map<K, V>::entries)
-}
+/**
+ * Creates an encoder that encodes a [Map] of objects of type [K] and [V]. The created encoder will encode each entry
+ * in the map as a [Pair] using the original encoder, and then encode the [endMarker] using the original encoder. The
+ * end marker is used to indicate the end of the map.
+ *
+ * @receiver the original encoder of [Pairs][Pair] of [K] and [V]
+ * @param endMarker the end marker that is used to indicate the end of the map
+ * @return an encoder that encodes a [Map] of objects of type [K] and [V]
+ */
+fun <K, V> Encoder<Pair<K, V>>.toMap(endMarker: Pair<K, V>): Encoder<Map<K, V>> =
+    Encoder { obj, output ->
+        obj.forEach { (key, value) ->
+            this@toMap.encode(key to value, output)
+        }
+        this@toMap.encode(endMarker, output)
+    }
 
+/**
+ * Creates an encoder that encodes a [Map] of objects of type [K] and [V]. The created encoder will encode the size of
+ * the map using the given [sizeEncoder], and then encode each key and value in the map using the original encoder for
+ * keys and the given [valueEncoder] for values.
+ *
+ * @receiver the original encoder of keys of type [K]
+ * @param valueEncoder the encoder that is used to encode the values of the map
+ * @param sizeEncoder the encoder that is used to encode the size of the map (defaults to the default [IntEncoder])
+ * @return an encoder that encodes a [Map] of objects of type [K] and [V]
+ */
 fun <K, V> Encoder<K>.toMap(valueEncoder: Encoder<V>, sizeEncoder: Encoder<Int> = IntEncoder()): Encoder<Map<K, V>> =
-    toMapEntryEncoder(valueEncoder).toMap(sizeEncoder)
+    Encoder { obj, output ->
+        sizeEncoder.encode(obj.size, output)
+        obj.forEach { (key, value) ->
+            this@toMap.encode(key, output)
+            valueEncoder.encode(value, output)
+        }
+    }
 
-fun <K, V> Encoder<K>.toMap(valueEncoder: Encoder<V>, endMarker: Pair<K, V>): Encoder<Map<K, V>> {
-    return toMapEntryEncoder(valueEncoder).toMap(endMarker)
-}
+/**
+ * Creates an encoder that encodes a [Map] of objects of type [K] and [V]. The created encoder will encode each key and
+ * value in the map using the original encoder for keys and the given [valueEncoder] for values, and then encode the
+ * [endMarker] in the same way. The end marker is used to indicate the end of the map.
+ *
+ * @receiver the original encoder of keys of type [K]
+ * @param valueEncoder the encoder that is used to encode the values of the map
+ * @param endMarker the end marker that is used to indicate the end of the map
+ * @return an encoder that encodes a [Map] of objects of type [K] and [V]
+ */
+fun <K, V> Encoder<K>.toMap(valueEncoder: Encoder<V>, endMarker: Pair<K, V>): Encoder<Map<K, V>> =
+    Encoder { obj, output ->
+        obj.forEach { (key, value) ->
+            this@toMap.encode(key, output)
+            valueEncoder.encode(value, output)
+        }
+        this@toMap.encode(endMarker.first, output)
+        valueEncoder.encode(endMarker.second, output)
+    }
 
+/**
+ * Creates an encoder that encodes an [Array] of objects of type [T]. The created encoder will encode the size of the
+ * array using the given [sizeEncoder], and then encode each object in the array using the original encoder.
+ *
+ * @receiver the original encoder of type [T]
+ * @param sizeEncoder the encoder that is used to encode the size of the array (defaults to the default [IntEncoder])
+ * @return an encoder that encodes an [Array] of objects of type [T]
+ */
+@JvmOverloads
+fun <T> Encoder<T>.toArray(sizeEncoder: Encoder<Int> = IntEncoder()): Encoder<Array<T>> =
+    Encoder { obj, output ->
+        sizeEncoder.encode(obj.size, output)
+        obj.forEach { this@toArray.encode(it, output) }
+    }
+
+/**
+ * Creates an encoder that encodes a nullable [T] object. The created encoder will encode a [Boolean] indicating
+ * whether the object is null or not, and then encode the object using the original encoder if it is not null.
+ *
+ * @receiver the original encoder of type [T]
+ * @param nullabilityEncoder the encoder that is used to encode the nullability of the object (defaults to the
+ * default [BooleanEncoder])
+ * @return an encoder that encodes a nullable [T] object
+ */
 @JvmOverloads
 fun <T : Any> Encoder<T>.toOptional(nullabilityEncoder: Encoder<Boolean> = BooleanEncoder()): Encoder<T?> =
     Encoder { obj, output ->
@@ -43,23 +163,3 @@ fun <T : Any> Encoder<T>.toOptional(nullabilityEncoder: Encoder<Boolean> = Boole
             this@toOptional.encode(obj!!, output)
         }
     }
-
-@JvmOverloads
-fun <T> Encoder<T>.toArray(sizeEncoder: Encoder<Int> = IntEncoder()): Encoder<Array<T>> =
-    Encoder { obj, output ->
-        sizeEncoder.encode(obj.size, output)
-        obj.forEach { this@toArray.encode(it, output) }
-    }
-
-fun <K, V> Encoder<K>.toMapEntryEncoder(valueEncoder: Encoder<V>): Encoder<Map.Entry<K, V>> =
-    Encoder { obj, output ->
-        this@toMapEntryEncoder.encode(obj.key, output)
-        valueEncoder.encode(obj.value, output)
-    }
-
-private fun <K, V> Pair<K, V>.toEntry(): Map.Entry<K, V> = object : Map.Entry<K, V> {
-    override val key: K
-        get() = first
-    override val value: V
-        get() = second
-}
