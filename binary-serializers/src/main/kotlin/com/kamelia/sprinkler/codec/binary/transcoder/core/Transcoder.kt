f25e0c8abc96edad0f502c2da2a9ee9b47a35012
ZwenDo <lorris.creantor@gmail.com>
2023-06-06 18:04:26 +0200

    Added Transcoder

---
+++ binary-serializers/src/main/kotlin/com/kamelia/sprinkler/codec/binary/transcoder/core/Transcoder.kt
@@ -0,0 +1,30 @@
+package com.kamelia.sprinkler.codec.binary.transcoder.core
+
+import com.kamelia.sprinkler.codec.binary.decoder.core.Decoder
+import com.kamelia.sprinkler.codec.binary.encoder.core.Encoder
+
+/**
+ * Represents an object that can both encode and decode values of type [T].
+ *
+ * @param T the type of values that can be encoded and decoded
+ * @see Encoder
+ * @see Decoder
+ */
+interface Transcoder<T> : Encoder<T>, Decoder<T> {
+
+    companion object {
+
+        /**
+         * Creates a [Transcoder] from the given [encoder] and [decoder].
+         *
+         * @param encoder the encoder used to encode values of type [T]
+         * @param decoder the decoder used to decode values of type [T]
+         * @param T the type of values that can be encoded and decoded
+         */
+        @JvmStatic
+        fun <T> create(encoder: Encoder<T>, decoder: Decoder<T>): Transcoder<T> =
+            object : Transcoder<T>, Encoder<T> by encoder, Decoder<T> by decoder {}
+
+    }
+
+}
