e3ddef991b018ad2b5ee43b46eae143af622f612
ZwenDo <lorris.creantor@gmail.com>
2023-07-13 13:16:15 +0200

    Disabled publish (maybe ?)

--- benchmarks/build.gradle.kts
+++ benchmarks/build.gradle.kts
@@ -4,3 +4,15 @@ subprojects {
         implementation("org.openjdk.jmh", "jmh-generator-annprocess", "1.36")
     }
 }
+
+allprojects {
+    // Disable publishing for all projects
+    tasks {
+        publish.configure {
+            enabled = false
+        }
+        publishToMavenLocal.configure {
+            enabled = false
+        }
+    }
+}
