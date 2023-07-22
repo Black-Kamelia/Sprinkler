25b486d7240c4df144d8de36498af12ba6ccc6e6
ZwenDo <lorris.creantor@gmail.com>
2023-07-22 14:50:18 +0200

    Uncommented benchmarks

--- binary-transcoders/src/jmh/kotlin/com/kamelia/benchmark/sprinkler/transcoder/binary/BasicInMemoryEncodingBenchmarks.kt
+++ binary-transcoders/src/jmh/kotlin/com/kamelia/benchmark/sprinkler/transcoder/binary/BasicInMemoryEncodingBenchmarks.kt
@@ -4,20 +4,21 @@ import com.kamelia.benchmark.sprinkler.transcoder.binary.`object`.BasicPerson
 import com.kamelia.benchmark.sprinkler.transcoder.binary.`object`.BasicPersonEncoder
 import com.kamelia.benchmark.sprinkler.transcoder.binary.`object`.basicPersonEncoder
 import com.kamelia.sprinkler.transcoder.binary.encoder.core.Encoder
+import org.openjdk.jmh.annotations.Benchmark
 import org.openjdk.jmh.annotations.Scope
 import org.openjdk.jmh.annotations.State
 
 open class BasicInMemoryEncodingBenchmarks {
 
-//    @Benchmark
-//    fun handmadeEncoderEncoding(state: BasicPersonEncodingState) {
-//        state.handmade.encode(state.person)
-//    }
-//
-//    @Benchmark
-//    fun compositionEncoderEncoding(state: BasicPersonEncodingState) {
-//        state.composition.encode(state.person)
-//    }
+    @Benchmark
+    fun handmadeEncoderEncoding(state: BasicPersonEncodingState) {
+        state.handmade.encode(state.person)
+    }
+
+    @Benchmark
+    fun compositionEncoderEncoding(state: BasicPersonEncodingState) {
+        state.composition.encode(state.person)
+    }
 
 }
 
