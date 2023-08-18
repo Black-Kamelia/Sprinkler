397cfaef105a1210166f789d41ad0de10ff3d87d
ZwenDo <lorris.creantor@gmail.com>
2023-08-18 21:44:32 +0200

    Added castOrNull

--- utils/src/main/kotlin/com/kamelia/sprinkler/util/Typing.kt
+++ utils/src/main/kotlin/com/kamelia/sprinkler/util/Typing.kt
@@ -8,3 +8,13 @@ package com.kamelia.sprinkler.util
  */
 @Suppress("UNCHECKED_CAST", "NOTHING_TO_INLINE")
 inline fun <T> Any?.unsafeCast(): T = this as T
+
+/**
+ * Tries to this nullable object to the specified type [T].
+ * If the cast fails, returns null.
+ *
+ * @receiver the object to cast or null
+ * @return the object cast as [T] or null if the cast fails
+ */
+@Suppress("UNCHECKED_CAST", "NOTHING_TO_INLINE")
+inline fun <T> Any?.castOrNull(): T? = this as? T
