a493f15c1602bd0d18bac08e2c831fbf53dfdf7f
ZwenDo <lorris.creantor@gmail.com>
2023-08-23 21:30:18 +0200

    Simplified structure

--- i18n/src/main/kotlin/com/kamelia/sprinkler/i18n/TranslationNode.kt
+++ i18n/src/main/kotlin/com/kamelia/sprinkler/i18n/TranslationNode.kt
@@ -7,8 +7,18 @@ sealed interface TranslationNode {
 
     class Inner(internal val children: Map<String, TranslationNode>) : TranslationNode {
 
-        fun getNode(key: String): TranslationNode = getTranslationNode(key, this)
-            ?: illegalArgument("No node found with key $key")
+        fun getNode(key: String): TranslationNode {
+            val fastResult = children[key]
+            if (fastResult != null) return fastResult
+
+            val keyParts = key.split('.')
+            var current: TranslationNode = this
+            for (keyPart in keyParts) {
+                current = current.castOrNull<Inner>()?.children?.get(keyPart)
+                    ?: illegalArgument("No node found with key $key")
+            }
+            return current
+        }
 
         fun section(key: String): Inner = getNode(key).castOrNull<Inner>()
             ?: illegalArgument("No section found with key $key")
@@ -20,29 +30,4 @@ sealed interface TranslationNode {
 
     class Leaf(val value: String) : TranslationNode
 
-}
-
-interface TranslationTree {
-
-    operator fun get(key: String): TranslationNode?
-
-}
-
-private class TranslationTreeImpl(private val root: TranslationNode.Inner) : TranslationTree {
-
-    override fun get(key: String): TranslationNode? = getTranslationNode(key, root)
-
-}
-
-
-private fun getTranslationNode(key: String, root: TranslationNode.Inner): TranslationNode? {
-    val fastResult = root.children[key]
-    if (fastResult != null) return fastResult
-
-    val keyParts = key.split('.')
-    var current: TranslationNode? = root
-    for (keyPart in keyParts) {
-        current = current.castOrNull<TranslationNode.Inner>()?.children?.get(keyPart) ?: return null
-    }
-    return current
 }
