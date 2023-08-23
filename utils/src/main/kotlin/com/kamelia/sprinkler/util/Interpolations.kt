20be7063950de8e098e897c0a4bd69faabf8365f
ZwenDo <lorris.creantor@gmail.com>
2023-08-23 15:07:46 +0200

    Added doc for NameResolver

--- utils/src/main/kotlin/com/kamelia/sprinkler/util/Interpolations.kt
+++ utils/src/main/kotlin/com/kamelia/sprinkler/util/Interpolations.kt
@@ -53,27 +53,98 @@ fun String.interpolate(args: Map<String, Any>, fallback: String? = null): String
 fun String.interpolate(vararg args: Pair<String, Any>, fallback: String? = null): String =
     interpolate(NameResolver.create(args.toMap(), fallback))
 
+/**
+ * Interface for resolving variable names during string interpolation. This interface maps variable names to their
+ * values.
+ *
+ * @see [String.interpolate]
+ * @see [NameResolver.create]
+ */
 fun interface NameResolver {
 
+    /**
+     * Returns the value of the variable with the given [name].
+     *
+     * Implementations may throw an [IllegalArgumentException] if the variable is unknown, or return a default value.
+     *
+     * @param name the name of the variable
+     * @return the value of the variable
+     * @throws IllegalArgumentException if the variable is unknown
+     */
     fun value(name: String): String
 
     companion object {
 
-        fun create(vararg args: Any?): NameResolver = NameResolver { name ->
-            val index = name.toIntOrNull() ?: error("Invalid index '$name'")
-            if (index !in args.indices) {
-                throw IllegalArgumentException("index must be in between 0 and ${args.size}, but was $index")
+        /**
+         * Creates a [NameResolver] that resolves variables by their index in the given [array][args].
+         *
+         * The variable passed to [NameResolver.value] is parsed as an integer, and the value at the corresponding index
+         * in the [array][args] is returned. If name does not represent a valid integer, or if the index is out of
+         * bounds, an [IllegalArgumentException] is thrown.
+         *
+         * Example:
+         * ```kt
+         * val resolver = NameResolver.create("foo", "bar", "baz")
+         * val result = "Hello {0}, {2}, {1}".interpolate(resolver)
+         * println(result) // prints "Hello foo, baz, bar"
+         * ```
+         *
+         * @param args the array of values
+         * @return a [NameResolver] that resolves variables by their index in the given [array][args]
+         */
+        fun create(vararg args: Any?): NameResolver =
+            NameResolver { name ->
+                val index = name.toIntOrNull() ?: illegalArgument("Invalid index '$name'")
+                if (index !in args.indices) {
+                    illegalArgument("index must be in between 0 and ${args.size}, but was $index")
+                }
+                args[index].toString()
             }
-            args[index].toString()
-        }
 
+        /**
+         * Creates a [NameResolver] that resolves variables by their name in the given [map][args].
+         *
+         * The name of the variable passed to [NameResolver.value] is used as a key in the [map][args], and the value
+         * associated with that key is returned. If a variable is unknown, the [fallback] value is returned. If the
+         * [fallback] value is `null`, an [IllegalArgumentException] is thrown.
+         *
+         * Example:
+         * ```kt
+         * val resolver = NameResolver.create(mapOf("name" to "John", "age" to 42), fallback = "unknown")
+         * val result = "Hello {name}, you are {age} years old, and you live in {city}".interpolate(resolver)
+         * println(result) // prints "Hello John, you are 42 years old, and you live in unknown"
+         * ```
+         *
+         * @param args the map of values
+         * @param fallback the fallback value (defaults to `null`)
+         * @return a [NameResolver] that resolves variables by their name in the given [map][args]
+         */
         fun create(args: Map<String, Any>, fallback: String? = null): NameResolver =
-            NameResolver {
-                args[it]?.toString() ?: fallback ?: throw IllegalArgumentException("Unknown variable '$it'")
+            NameResolver { name ->
+                args[name]?.toString() ?: fallback ?: illegalArgument("Unknown variable '$name'")
             }
 
+        /**
+         * Creates a [NameResolver] that resolves variables by their name in the [Pair] array [args]. The array of pairs
+         * is converted to a [map][Map].
+         *
+         * The name of the variable passed to [NameResolver.value] is used as a key in the map created from the
+         * [array][args] and the value associated with that key is returned. If a variable is unknown, the [fallback]
+         * value is returned. If the [fallback] value is `null`, an [IllegalArgumentException] is thrown.
+         *
+         * Example:
+         * ```kt
+         * val resolver = NameResolver.create("name" to "John", "age" to 42, fallback = "unknown")
+         * val result = "Hello {name}, you are {age} years old, and you live in {city}".interpolate(resolver)
+         * println(result) // prints "Hello John, you are 42 years old, and you live in unknown"
+         * ```
+         *
+         * @param args the array of pairs
+         * @param fallback the fallback value (defaults to `null`)
+         * @return a [NameResolver] that resolves variables by their name in the [Pair] array [args]
+         */
         fun create(vararg args: Pair<String, Any>, fallback: String? = null): NameResolver =
-            create(args.toMap(), fallback = fallback)
+            create(args.toMap(), fallback)
 
     }
 
