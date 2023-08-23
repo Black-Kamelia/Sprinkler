552a936e46c45db623778e396b8a18ed552a783f
ZwenDo <lorris.creantor@gmail.com>
2023-08-23 14:36:56 +0200

    Added different overloads for the NameResolver interface

--- utils/src/main/kotlin/com/kamelia/sprinkler/util/Interpolations.kt
+++ utils/src/main/kotlin/com/kamelia/sprinkler/util/Interpolations.kt
@@ -45,9 +45,37 @@ fun String.interpolate(resolver: NameResolver): String {
     return builder.toString()
 }
 
+fun String.interpolate(vararg args: Any?): String = interpolate(NameResolver.create(*args))
+
+fun String.interpolate(args: Map<String, Any>, fallback: String? = null): String =
+    interpolate(NameResolver.create(args, fallback))
+
+fun String.interpolate(vararg args: Pair<String, Any>, fallback: String? = null): String =
+    interpolate(NameResolver.create(args.toMap(), fallback))
+
 fun interface NameResolver {
 
     fun value(name: String): String
+
+    companion object {
+
+        fun create(vararg args: Any?): NameResolver = NameResolver { name ->
+            val index = name.toIntOrNull() ?: error("Invalid index '$name'")
+            if (index !in args.indices) {
+                throw IllegalArgumentException("index must be in between 0 and ${args.size}, but was $index")
+            }
+            args[index].toString()
+        }
+
+        fun create(args: Map<String, Any>, fallback: String? = null): NameResolver =
+            NameResolver {
+                args[it]?.toString() ?: fallback ?: throw IllegalArgumentException("Unknown variable '$it'")
+            }
+
+        fun create(vararg args: Pair<String, Any>, fallback: String? = null): NameResolver =
+            create(args.toMap(), fallback = fallback)
+
+    }
 
 }
 
