70613987822ec44251f78cffb321813b44cd5a4f
ZwenDo <lorris.creantor@gmail.com>
2023-08-25 17:16:44 +0200

    Added toMap method

--- i18n/src/main/kotlin/com/kamelia/sprinkler/i18n/Translator.kt
+++ i18n/src/main/kotlin/com/kamelia/sprinkler/i18n/Translator.kt
@@ -1,7 +1,6 @@
 package com.kamelia.sprinkler.i18n
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.kamelia.sprinkler.util.castOrNull
 import com.kamelia.sprinkler.util.illegalArgument
 import com.kamelia.sprinkler.util.unsafeCast
 import org.yaml.snakeyaml.Yaml
@@ -12,11 +11,9 @@ import java.util.*
  * - key syntax
  * - accepted value types (string, map, list)
  * - flattening
- * - accept a functional interface in builder addPath methods that resolvers the locale using the path name
  *
- * - add a property which returns the root translator => reference to the root in subsections
- * - map method that returns a copy of the inner map ? (does it filter only the keys of the section represented by the
- *  translator ?)
+ *
+ * - accept a functional interface in builder addPath methods that resolvers the locale using the path name
  */
 interface Translator {
 
@@ -33,6 +30,8 @@ interface Translator {
 
     fun t(key: String, locale: Locale): String = translate(key, locale)
 
+    fun toMap(): Map<Locale, Map<String, String>>
+
     companion object {
 
         @JvmStatic
@@ -45,10 +44,10 @@ interface Translator {
 internal class TranslatorImpl private constructor(
     override val rootKey: String?,
     override val defaultLocale: Locale,
-    private val translations: Map<Locale, Map<String, Any>>,
+    private val translations: Map<Locale, Map<String, String>>,
 ) : Translator {
 
-    constructor(defaultLocale: Locale, children: Map<Locale, Map<String, Any>>) : this(null, defaultLocale, children)
+    constructor(defaultLocale: Locale, children: Map<Locale, Map<String, String>>) : this(null, defaultLocale, children)
 
     override fun section(key: String): Translator {
         require(FULL_KEY_REGEX.matches(key)) {
@@ -56,6 +55,22 @@ internal class TranslatorImpl private constructor(
         }
         val newRootKey = rootKey?.let { "$it.$key" } ?: key
         return TranslatorImpl(newRootKey, defaultLocale, translations)
+    }
+
+    override fun toMap(): Map<Locale, Map<String, String>> {
+        if (isRoot) {
+            return buildMap {
+                translations.forEach { (locale, map) ->
+                    put(locale, map.toMap())
+                }
+            }
+        }
+        val root = rootKey!!
+        return buildMap {
+            translations.forEach { (locale, map) ->
+                put(locale, map.filter { (key, _) -> key.startsWith(root) })
+            }
+        }
     }
 
     override fun translate(key: String, locale: Locale): String {
@@ -72,7 +87,7 @@ internal class TranslatorImpl private constructor(
 
     private fun innerTranslate(key: String, locale: Locale): String {
         val localeMap = translations[locale] ?: return tryFallback(key, locale)
-        return localeMap[key].castOrNull<String>() ?: tryFallback(key, locale)
+        return localeMap[key] ?: tryFallback(key, locale)
     }
 
     private fun tryFallback(key: String, locale: Locale): String {
@@ -100,5 +115,4 @@ fun main() {
         .addMap(Locale.CHINESE, mapOf("color" to "bingchilling2"))
         .duplicateKeyResolutionPolicy(TranslatorBuilder.DuplicateKeyResolution.KEEP_FIRST)
         .build()
-    println(translator.translate("color", Locale.ENGLISH))
 }
