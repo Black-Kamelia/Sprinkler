28593454edb89572a53142f1c7d901af50ba4e3f
ZwenDo <lorris.creantor@gmail.com>
2023-08-30 10:26:44 +0200

    Removed parsing dependencies

--- i18n/build.gradle.kts
+++ i18n/build.gradle.kts
@@ -1,5 +1,3 @@
 dependencies {
     implementation(project(":utils"))
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.14.2")
-    implementation("org.yaml:snakeyaml:2.1")
 }
--- i18n/src/main/kotlin/com/kamelia/sprinkler/i18n/Internal.kt
+++ i18n/src/main/kotlin/com/kamelia/sprinkler/i18n/Internal.kt
@@ -1,13 +1,9 @@
 @file:PackagePrivate
+
 package com.kamelia.sprinkler.i18n
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.kamelia.sprinkler.util.unsafeCast
 import com.zwendo.restrikt.annotation.PackagePrivate
 import org.intellij.lang.annotations.Language
-import org.yaml.snakeyaml.Yaml
-import java.io.File
-import java.util.*
 
 @Language("RegExp")
 private const val KEY_IDENTIFIER_REGEX_STRING = """[a-zA-Z0-9](?:[\w-]*[a-zA-Z0-9])?"""
@@ -15,22 +11,3 @@ private const val KEY_IDENTIFIER_REGEX_STRING = """[a-zA-Z0-9](?:[\w-]*[a-zA-Z0-
 internal val KEY_IDENTIFIER_REGEX = KEY_IDENTIFIER_REGEX_STRING.toRegex()
 
 internal val FULL_KEY_REGEX = """$KEY_IDENTIFIER_REGEX_STRING(?:\.$KEY_IDENTIFIER_REGEX_STRING)*""".toRegex()
-
-fun jsonParser(): I18nFileParser = I18nFileParser.from { content ->
-    ObjectMapper().readValue(content, HashMap::class.java).unsafeCast()
-}
-
-fun yamlParser(): I18nFileParser = I18nFileParser.from { Yaml().load(it) }
-
-fun main() {
-    val translator = Translator.builder(Locale.FRANCE)
-        .addFile(File("translations"), yamlParser())
-        .build()
-    val translator2 = TranslatorBuilder(Locale.FRANCE)
-        .addFile(File("foo.json"), jsonParser()) { Locale.ENGLISH }
-        .build()
-    val str = translator.section("pages.login").prettyDisplay(Locale.ENGLISH)
-    val str2 = translator2.prettyDisplay(Locale.ENGLISH)
-    println(translator.toMap() == translator2.toMap())
-    println(str2)
-}
