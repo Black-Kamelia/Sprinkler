39210c82b5fe380135be7b166afabda5d38a121e
ZwenDo <lorris.creantor@gmail.com>
2023-09-08 10:02:21 +0200

    Extracted versions to gradle.properties

--- i18n/build.gradle.kts
+++ i18n/build.gradle.kts
@@ -1,5 +1,8 @@
+val orgJsonVersion: String by rootProject
+val snakeYamlVersion: String by rootProject
+
 dependencies {
     implementation(project(":utils"))
-    implementation("org.json:json:20230618")
-    implementation("org.yaml:snakeyaml:2.2")
+    implementation("org.json", "json", orgJsonVersion)
+    implementation("org.yaml", "snakeyaml", snakeYamlVersion)
 }
