b22a3c63afde80e6d50778ccdaa83320f7983b15
ZwenDo <lorris.creantor@gmail.com>
2023-09-11 09:11:15 +0200

    Added OptionConfiguration class

---
+++ i18n/src/main/kotlin/com/kamelia/sprinkler/i18n/OptionConfiguration.kt
@@ -0,0 +1,41 @@
+package com.kamelia.sprinkler.i18n
+
+import com.kamelia.sprinkler.bridge.KotlinDslAdapter
+import com.kamelia.sprinkler.util.VariableDelimiter
+import com.zwendo.restrikt.annotation.PackagePrivate
+import java.util.*
+
+class OptionConfiguration @PackagePrivate internal constructor(
+    internal val alwaysEnableNestedParsing: Boolean,
+    internal val nestingVariableDelimiter: VariableDelimiter,
+    internal val interpolationDelimiter: VariableDelimiter,
+    internal val pluralMapper: (Locale, Int) -> Options.Plurals,
+) {
+
+    companion object {
+
+        inline fun create(block: Builder.() -> Unit): OptionConfiguration = Builder().apply(block).build()
+
+    }
+
+    class Builder @PublishedApi internal constructor() : KotlinDslAdapter {
+
+        var alwaysEnableNestedParsing: Boolean = false
+
+        var nestingVariableDelimiter: VariableDelimiter = VariableDelimiter('[', ']')
+
+        var interpolationDelimiter: VariableDelimiter = VariableDelimiter.DEFAULT
+
+        var pluralMapper: (Locale, Int) -> Options.Plurals = Options.Plurals.Companion::defaultMapper
+
+        @PublishedApi
+        internal fun build(): OptionConfiguration = OptionConfiguration(
+            alwaysEnableNestedParsing,
+            nestingVariableDelimiter,
+            interpolationDelimiter,
+            pluralMapper,
+        )
+
+    }
+
+}
