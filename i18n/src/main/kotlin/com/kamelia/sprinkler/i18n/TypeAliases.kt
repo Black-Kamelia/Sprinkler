d88086b523e716624617eef5d01b712fdcc3e568
ZwenDo <lorris.creantor@gmail.com>
2023-12-11 18:53:04 +0100

    Updated TranslationSourceData doc

--- i18n/src/main/kotlin/com/kamelia/sprinkler/i18n/TypeAliases.kt
+++ i18n/src/main/kotlin/com/kamelia/sprinkler/i18n/TypeAliases.kt
@@ -31,9 +31,27 @@ typealias Identifier = String
  * Represents the syntax of a translation variable, however, it does not take into account the delimiters, but only the
  * characters within the delimiters.
  *
- * It should respect the following rules:
- * - Empty variables are not allowed
- * - It can only contain alphanumeric characters, dashes and underscores
+ * It should be structured as follows:
+ * - Always starts with an [Identifier] representing the name of the variable
+ * - Can be directly followed by ',' to indicate the presence of a format
+ * - An arbitrary number of spaces can be present between the ',' and the start of the format name
+ * - The format name must respect the [Identifier] rules
+ * - The format name can be directly followed by a '(' to indicate the presence of format parameters (always at least
+ * one)
+ * - Parameters are represented as key-value pairs, separated by ':' and can contain any character except ')', ':', and
+ * ',' (unless escaped),
+ * - Each parameter is separated by a (non-escaped) ','
+ * - The last parameter must be followed by a ')'
+ *
+ * Here are some examples of valid [TranslationInterpolationVariables][TranslationInterpolationVariable]:
+ * - `my-variable`
+ * - `my-variable,my-format`
+ * - `my-variable,   format`
+ * - `my-variable, my-format(param1:value1)`
+ * - `my-variable, my-format(param1\:still-name:value1,param2:value2)`
+ *
+ * Whenever a [String] is supposed to represent a [TranslationInterpolationVariable], and does not respect the rules
+ * above, it will result in an exception being thrown (the type of the exception will depend on the function).
  */
 typealias TranslationInterpolationVariable = String
 
@@ -63,7 +81,7 @@ typealias TranslationKey = String
  * Represents the data stored in a [TranslationSourceMap], used to build a [Translator], using a [TranslatorBuilder].
  *
  * It can be one of the following types:
- * - [String]
+ * - [TranslationInterpolationVariable]
  * - [Boolean]
  * - Subtype of [Number] (e.g. [Int], [Long], [Double], etc.)
  * - [List] of [TranslationSourceData]
