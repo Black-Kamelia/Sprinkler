df7aeaf55c94fe256969793b0acf9888c2e83467
ZwenDo <lorris.creantor@gmail.com>
2024-03-12 19:08:21 +0100

    Hid utils VariableDelimiter dependency in TranslatorConfiguration

--- i18n/src/main/kotlin/com/kamelia/sprinkler/i18n/TranslatorConfiguration.kt
+++ i18n/src/main/kotlin/com/kamelia/sprinkler/i18n/TranslatorConfiguration.kt
@@ -3,6 +3,8 @@ package com.kamelia.sprinkler.i18n
 import com.kamelia.sprinkler.bridge.KotlinDslAdapter
 import com.kamelia.sprinkler.i18n.TranslatorConfiguration.Companion.create
 import com.kamelia.sprinkler.util.VariableDelimiter
+import com.zwendo.restrikt.annotation.HideFromJava
+import com.zwendo.restrikt.annotation.HideFromKotlin
 import com.zwendo.restrikt.annotation.PackagePrivate
 import java.util.Locale
 
@@ -41,6 +43,46 @@ class TranslatorConfiguration @PackagePrivate internal constructor(
 
     }
 
+    /**
+     * Delimiter to use for interpolation in translations.
+     */
+    class InterpolationDelimiter @PackagePrivate internal constructor(
+        internal val inner: VariableDelimiter,
+    ) {
+
+        companion object {
+
+            /**
+             * Creates an [InterpolationDelimiter] using the given [start] and [end] delimiters.
+             *
+             * The delimiters cannot contain the following characters: `\`, `(`, `)`, `:`. Trying to create a delimiter
+             * containing one of these characters will throw an [IllegalStateException].
+             *
+             * @param start the start delimiter
+             * @param end the end delimiter
+             * @return the created [InterpolationDelimiter]
+             * @throws IllegalStateException if the delimiters contain forbidden characters
+             */
+            @JvmStatic
+            fun create(start: String, end: String): InterpolationDelimiter {
+                val inner = VariableDelimiter.create(start, end)
+                val ch = forbiddenChars()
+                val forbiddenChars = ch.joinToString("", "[^", "]*") { it.toString() }.toRegex()
+                check(forbiddenChars.matches(inner.startDelimiter)) {
+                    "Start delimiter cannot contain the following characters: ${ch.contentToString()}, but was '${inner.startDelimiter}'"
+                }
+                check(forbiddenChars.matches(inner.endDelimiter)) {
+                    "End delimiter cannot contain the following characters: ${ch.contentToString()}, but was '${inner.endDelimiter}'"
+                }
+                return InterpolationDelimiter(inner)
+            }
+
+            private fun forbiddenChars(): CharArray = charArrayOf('\\', '(', ')', ':')
+
+        }
+
+    }
+
     companion object {
 
         /**
@@ -52,8 +94,6 @@ class TranslatorConfiguration @PackagePrivate internal constructor(
         @JvmStatic
         fun create(block: Builder.() -> Unit): TranslatorConfiguration = Builder().apply(block).build()
 
-        private fun forbiddenChars(): CharArray = charArrayOf('\\', '(', ')', ':')
-
     }
 
     /**
@@ -64,12 +104,10 @@ class TranslatorConfiguration @PackagePrivate internal constructor(
         /**
          * The delimiter to use for interpolation in translations.
          *
-         * The delimiter cannot contain the following characters: `\`, `(`, `)`, `:`. Any delimiter containing one of
-         * these characters will throw an [IllegalStateException] when [build] is called.
-         *
-         * default: [VariableDelimiter.default]
+         * default: '{{' and '}}'
          */
-        var interpolationDelimiter: VariableDelimiter = VariableDelimiter.default
+        @set:HideFromJava
+        var interpolationDelimiter: InterpolationDelimiter = InterpolationDelimiter(VariableDelimiter.default)
 
         /**
          * The mapper function to use for the pluralization strategy.
@@ -77,6 +115,7 @@ class TranslatorConfiguration @PackagePrivate internal constructor(
          *
          * default: [Plural.defaultMapper]
          */
+        @set:HideFromJava
         var pluralMapper: Plural.Mapper = Plural.defaultMapper()
 
         /**
@@ -86,6 +125,7 @@ class TranslatorConfiguration @PackagePrivate internal constructor(
          *
          * @see VariableFormatter
          */
+        @set:HideFromJava
         var formats: Map<String, VariableFormatter> = VariableFormatter.builtins()
 
         /**
@@ -95,26 +135,64 @@ class TranslatorConfiguration @PackagePrivate internal constructor(
          *
          * @see MissingKeyPolicy
          */
+        @set:HideFromJava
         var missingKeyPolicy: MissingKeyPolicy = MissingKeyPolicy.THROW_EXCEPTION
 
-        @PackagePrivate
-        internal fun build(): TranslatorConfiguration {
-            val ch = forbiddenChars()
-            val forbiddenChars = ch.joinToString("", "[^", "]*") { it.toString() }.toRegex()
-            check(forbiddenChars.matches(interpolationDelimiter.startDelimiter)) {
-                "Start delimiter cannot contain the following characters: ${ch.contentToString()}, but was '${interpolationDelimiter.startDelimiter}'"
-            }
-            check(forbiddenChars.matches(interpolationDelimiter.endDelimiter)) {
-                "End delimiter cannot contain the following characters: ${ch.contentToString()}, but was '${interpolationDelimiter.endDelimiter}'"
-            }
+        /**
+         * Sets the delimiter to use for interpolation in translations.
+         *
+         * The delimiter cannot contain the following characters: `\`, `(`, `)`, `:`. Any delimiter containing one of
+         * these characters will throw an [IllegalStateException] when [build] is called.
+         *
+         * default: '{{' and '}}'
+         *
+         * @param value the delimiter to use
+         * @return this [Builder]
+         */
+        @HideFromKotlin
+        fun setInterpolationDelimiter(value: InterpolationDelimiter): Builder = apply { interpolationDelimiter = value }
 
-            return TranslatorConfiguration(
-                interpolationDelimiter,
-                pluralMapper,
-                formats.toMap(),
-                missingKeyPolicy,
-            )
-        }
+        /**
+         * Sets the mapper function to use for the pluralization strategy.
+         * Said strategy can use a given [Locale] and count to return a [Plural] value.
+         *
+         * default: [Plural.defaultMapper]
+         *
+         * @param value the mapper function to use
+         * @return this [Builder]
+         */
+        @HideFromKotlin
+        fun setPluralMapper(value: Plural.Mapper): Builder = apply { pluralMapper = value }
+
+        /**
+         * Sets the map used to find formatters using their name during variable interpolation.
+         *
+         * default: [VariableFormatter.builtins]
+         *
+         * @param value the map to use
+         * @return this [Builder]
+         */
+        @HideFromKotlin
+        fun setFormats(value: Map<String, VariableFormatter>): Builder = apply { formats = value }
+
+        /**
+         * Sets the policy to use when a key is not found.
+         *
+         * default: [MissingKeyPolicy.THROW_EXCEPTION]
+         *
+         * @param value the policy to use
+         * @return this [Builder]
+         */
+        @HideFromKotlin
+        fun setMissingKeyPolicy(value: MissingKeyPolicy): Builder = apply { missingKeyPolicy = value }
+
+        @PackagePrivate
+        internal fun build(): TranslatorConfiguration = TranslatorConfiguration(
+            interpolationDelimiter.inner,
+            pluralMapper,
+            formats.toMap(),
+            missingKeyPolicy,
+        )
 
     }
 
@@ -122,4 +200,3 @@ class TranslatorConfiguration @PackagePrivate internal constructor(
         "TranslatorConfiguration(interpolationDelimiter=$interpolationDelimiter, pluralMapper=$pluralMapper, formats=$formats, missingKeyPolicy=$missingKeyPolicy)"
 
 }
-
